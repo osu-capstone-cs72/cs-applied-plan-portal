cf505ad45e283488c4b08ebef9e84679353f94ff
silverware13 <thomasza@oregonstate.edu>
2020-01-04 21:47:47 -0800

    Created server.js and index.js that allow submission of forms.

---
+++ public/index.js
@@ -0,0 +1,82 @@
+console.log("JavaScript start");
+
+// define variable for the "Submit Plan" button
+var submitButton = document.getElementById('submit-button');
+
+// define variables for the input text values
+var userId, course1, course2, course3, course4, course5, course6, course7,
+course8, course9, course10, course11, course12 = "";
+
+// assign all of the input values to variables
+function getInputs() {
+    userId = document.getElementById('user-id').value;
+    course1 = document.getElementById('course-1').value;
+    course2 = document.getElementById('course-2').value;
+    course3 = document.getElementById('course-3').value;
+    course4 = document.getElementById('course-4').value;
+    course5 = document.getElementById('course-5').value;
+    course6 = document.getElementById('course-6').value;
+    course7 = document.getElementById('course-7').value;
+    course8 = document.getElementById('course-8').value;
+    course9 = document.getElementById('course-9').value;
+    course10 = document.getElementById('course-10').value;
+    course11 = document.getElementById('course-11').value;
+    course12 = document.getElementById('course-12').value;
+}
+
+// clear all of the input fields for the form
+function clearInputs() {
+    document.getElementById('plan-form').reset();
+}
+
+// submit the users plan
+function submitPlan(userId, course1, course2, course3, course4, course5,
+    course6, course7, course8, course9, course10, course11, course12) {
+
+    var postRequest = new XMLHttpRequest();
+    var postURL = "/appliedplanportal/form";
+    postRequest.open('POST', postURL);
+
+    var postObj = {
+        userId: userId,
+        course1: course1,
+        course2: course2,
+        course3: course3,
+        course4: course4,
+        course5: course5,
+        course6: course6,
+        course7: course7,
+        course8: course8,
+        course9: course9,
+        course10: course10,
+        course11: course11,
+        course12: course12
+    };
+
+    var requestBody = JSON.stringify(postObj);
+    postRequest.setRequestHeader('Content-Type', 'application/json');
+
+    postRequest.addEventListener('load', function (event) {
+        if (event.target.status !== 200) {
+            alert("Error storing plan in database:\n" + event.target.response);
+        } else {
+            console.log("Plan submitted.");
+        }
+    });
+
+    postRequest.send(requestBody);
+    console.log("postRequest:", requestBody);
+    alert("Request:\n" + requestBody);
+}
+
+// user clicks on the "Submit Plan" button
+submitButton.addEventListener("click", function(e) {
+    console.log("Submit button pressed.");
+    getInputs();
+    submitPlan(userId, course1, course2, course3, course4, course5, course6,
+        course7, course8, course9, course10, course11, course12);
+    clearInputs();
+});
+
+// update select to include new options
+function updateSelect(newOptions) {}
--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 console.log("Server JavaScript start");
+var bodyParser = require('body-parser');
 var express = require('express');
+var mysql = require('mysql');
+var path = require('path');
+var fs = require('fs');
 var app = express();
 var port = process.env.PORT || 3306;
 
-/////////////////////////////
-////*Connect to database*////
-/////////////////////////////
-
-var mysql = require("mysql");
-
-// database config using env variables
+// create database connection using environment variables
 var con = mysql.createConnection({
     //    host: process.env.SQL_HOST,
     //    user: process.env.SQL_USER,
@@ -27,24 +25,44 @@ con.connect(function (err) {
     if (err) console.log(err);
     console.log("Connected to database");
 
-    // query the database to get user info
-    con.query('select * from User', function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-    });
-
-    // query the database to get the course info
-    con.query('select * from Course', function (err, result, fields) {
-        if (err) throw err;
-        console.log(result);
-    });
-
 });
 
 //////////////////////////////////////
 ////*Express Middleware Functions*////
 //////////////////////////////////////
 
+// parse request bodies as JSON
+app.use(bodyParser.json());
+
+// submit the user's plan to the database
+app.post('/appliedplanportal/form', function (req, res, next) {
+
+    con.query('select firstName, lastName from User', function (err, result, fields) {
+        if (err) {
+            res.status(500).send("Error saving plan");
+            throw err;
+        } else {
+            res.status(200).send("Plan saved");
+            console.log(result);
+        }
+    });
+
+});
+
+app.use(express.static('public'));
+
+// user has requested the homepage
+app.get('/', function (req, res, next) {
+    console.log("Homepage")
+});
+
+// everything else gets a 404 error
+app.get('*', function (req, res, next) {
+    res.status(404).send;
+    console.log("404");
+});
+
+// listen on the current port
 var server = app.listen(port, function () {
-    console.log("Server is listening on port ", port);
+    console.log("Server is listening on port", port);
 });
