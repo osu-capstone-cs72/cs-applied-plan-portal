c5bc8cdeea6f491b32f0a99b68d81948fd7cc274
clairecahill <clairecahill@users.noreply.github.com>
2020-01-18 15:51:57 -0800

    Add filter bar and more courses

--- src/components/Course.js
+++ src/components/Course.js
@@ -23,12 +23,14 @@ export default class Course extends React.Component {
         return(
         <div className="course">
             <p>{this.props.code} - {this.props.title}</p>
-            {this.state.full && <p>Credit hours: {this.props.hours}</p>} 
+            {this.state.full && <p>Credit hours: {this.props.credits}</p>} 
             {this.state.full && <p>{this.props.description}</p>} 
             {this.state.full && <p>Prerequisites: {this.props.prereqs}</p>}
-            <button className="btn btn-add">+ Add to plan</button> 
-            {this.state.full ? <button onClick={this.arrowButton}><i className="fad fa-angle-double-up"></i></button>
-            : <button onClick={this.arrowButton}><i className="fad fa-angle-double-down"></i></button>}
+            <div className="course-btn-container">
+                <button className="btn btn-add">+ Add to plan</button> 
+                {this.state.full ? <button className="expand-btn" onClick={this.arrowButton}><i className="fad fa-angle-double-up"></i>-</button>
+                : <button className="expand-btn" onClick={this.arrowButton}><i className="fad fa-angle-double-down"></i>+</button>}
+            </div>
         </div>
         );
     }
--- src/components/CourseContainer.js
+++ src/components/CourseContainer.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Course from './Course'
+import FilterBar from './FilterBar'
 import courses from './CourseList'
+import filters from './FilterList'
 import '../public/index.css'
 
 export default class CourseContainer extends React.Component {
@@ -9,11 +11,13 @@ export default class CourseContainer extends React.Component {
 
         this.state = {
             initialCourses: [],
-            courses: []
+            courses: [],
+            filter: ""
         }
 
         this.loadCourses = this.loadCourses.bind(this);
         this.filterSearch = this.filterSearch.bind(this);
+        this.handleFilterChange = this.handleFilterChange.bind(this);
     }
 
     loadCourses(){
@@ -33,7 +37,22 @@ export default class CourseContainer extends React.Component {
         });
         this.setState({
             courses: courses
-        })
+        });
+    }
+
+    handleFilterChange(value){
+        this.setState({
+            filter: value
+        });
+        let courses = this.state.initialCourses;
+        courses = courses.filter(c => {
+            return (
+                c.code.startsWith(value)
+            );
+        });
+        this.setState({
+            courses: courses
+        });
     }
 
     componentWillMount(){
@@ -43,14 +62,19 @@ export default class CourseContainer extends React.Component {
     render(){
         return(
         <div className="course-container">
-            <div className="search-container">
-                <form className="form-inline my-2 my-lg-0">
-                    <input className="form-control mr-sm-2" type="text" placeholder="Search.." name="search" onChange={this.filterSearch}/>
-                    <button className="btn btn-outline-success my-2 my-sm-0" type="submit"><i className="fa fa-search"></i></button>
+            <div className="top-bar">
+                <div className="search-container">
+                    <form className="form-inline my-2 my-lg-0">
+                        <input className="form-control mr-sm-2" type="text" placeholder="Search.." name="search" onChange={this.filterSearch}/>
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit"><i className="fa fa-search"></i></button>
+                    </form>
+                </div>
+                <form className="course-filter form-group">
+                    <FilterBar options={filters} value={this.state.filter} onValueChange={this.handleFilterChange}/>
                 </form>
             </div>
             <div className="explore-courses">
-                {this.state.courses.map(c => <Course key={c} code={c.code} title={c.title} hours={c.hours} 
+                {this.state.courses.map(c => <Course key={c} code={c.code} title={c.title} credits={c.credits} 
                 description={c.description} prereqs={c.prereqs} />) }
             </div>
         </div>
