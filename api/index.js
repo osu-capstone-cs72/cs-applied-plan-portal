d0ed6e1c49773b98c4b7749a8794248a1ef43c8d
silverware13 <thomasza@oregonstate.edu>
2020-01-23 07:55:43 -0800

    Update the directory that files are served from

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ app.use("/plan", require("./plan"));
 app.use(bodyParser.json());
 
 // statically serve files from the public directory
-app.use(express.static("views/public"));
+app.use(express.static("src/public"));
 
 // everything else gets a 404 error
 app.get("*", (req, res) => {
