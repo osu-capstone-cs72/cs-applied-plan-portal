07709364e21a565ec296adea8aa079987e301084
JacksGo <jackson.golletz@gmail.com>
2020-01-22 16:21:52 -0800

    Set default listener port for Express
    Fixes #14

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require("./utils/mysqlPool").pool;
 const app = require("./api/index");
 
 // listen for incoming requests
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log("Server is listening on port", port, "\n");
 });
