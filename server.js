329d45c4ef26c58ae2c465263338b2327f4a3bd4
silverware13 <thomasza@oregonstate.edu>
2020-01-23 07:44:00 -0800

    Update server.js to match the organize-structure branch

--- server.js
+++ server.js
@@ -5,11 +5,25 @@ console.log("Server JavaScript start");
 
 // setup database connection and routing
 require("dotenv").config();
-require("./utils/mysqlPool").pool;
+const pool = require("./utils/mysqlPool").pool;
 const app = require("./api/index");
 
+// confirm that connection was made to the database
+function testConnection(pool, callback) {
+  pool.query("SELECT courseId FROM Course WHERE courseId = 1", (err) => {
+    if (err) {
+      throw err;
+    } else {
+      console.log("Connected to database");
+      callback();
+    }
+  });
+}
+
 // listen for incoming requests
-const port = process.env.PORT;
-app.listen(port, () => {
-  console.log("Server is listening on port", port, "\n");
+const port = process.env.PORT || 3000;
+testConnection(pool, () => {
+  app.listen(port, () => {
+    console.log("Server is listening on port", port, "\n");
+  });
 });
