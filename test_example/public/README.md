782efcc4c947f7d9e4dc5edfcad4c3dfa21f53a5
silverware13 <thomasza@oregonstate.edu>
2020-01-24 19:57:37 -0800

    Add temporary example for making a course GET request

---
+++ test_example/public/README.md
@@ -0,0 +1,2 @@
+Please remove this directory (and its contents) after you are
+done implementing the real submit/edit plan page.
---
+++ test_example/public/index.js
@@ -0,0 +1,53 @@
+console.log("JavaScript start");
+
+// define constant for the "Submit Plan" button
+const submitButton = document.getElementById("submit-button");
+
+// define variables for the input text values
+let courseCode = "";
+
+// assign all of the form text values to variables
+function getInputs() {
+  courseCode = document.getElementById("course-code").value;
+}
+
+// clear all of the input text fields of the form
+function clearInputs() {
+  document.getElementById("plan-form").reset();
+}
+
+// submit the users plan
+function submitPlan(courseCode) {
+
+  const postRequest = new XMLHttpRequest();
+  const postURL = "/course";
+  postRequest.open("GET", postURL);
+
+  const postObj = {
+    courseCode: courseCode
+  };
+
+  const requestBody = JSON.stringify(postObj);
+  postRequest.setRequestHeader("Content-Type", "application/json");
+
+  postRequest.addEventListener("load", (event) => {
+    if (event.target.status !== 200) {
+      alert("Error submitting plan:\n" + event.target.response);
+    } else {
+      alert("Response:\n" + event.target.response);
+      const obj = JSON.parse(event.target.response);
+      console.log(obj.credits, obj.courseName, obj.courseCode, obj.restriction);
+      clearInputs();
+    }
+  });
+
+  postRequest.send(requestBody);
+  console.log("postRequest:", requestBody);
+}
+
+// user clicks on the "Submit Plan" button
+submitButton.addEventListener("click", () => {
+  console.log("Submit button pressed");
+  getInputs();
+  submitPlan(courseCode);
+});
