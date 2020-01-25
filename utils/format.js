0e0417c4ba1f9a93c59c81743aa42ce54132f987
silverware13 <thomasza@oregonstate.edu>
2020-01-25 10:05:07 -0800

    Add function to format individual strings

--- utils/format.js
+++ utils/format.js
@@ -1,6 +1,13 @@
 // File: format.js
 // Description: handles formating for various datatypes
 
+// takes a string and makes it uppercase and free of white space
+module.exports = function formatString(string) {
+
+  return string.toUpperCase().replace(/\s+/g, "");
+
+};
+
 // takes an array of strings and makes them uppercase, free of white space,
 // and removes empty strings from the array
 module.exports = function formatStringArray(stringArray) {
