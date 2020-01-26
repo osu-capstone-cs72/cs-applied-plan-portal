78015471a075aeba6fc4cd421d9342a9b645e717
Phi Luu <philectron@users.noreply.github.com>
2020-01-25 21:46:09 -0800

    Add a more general validator and sanitizer
    The new validator and sanitizer functions have not been integrated to
    the API handler yet. It hasn't been tested either.

--- utils/validation.js
+++ utils/validation.js
@@ -1,13 +1,96 @@
 // File: validation.js
 // Description: validates a submitted form against a list of constraints
 
+const validator = require("validator");
+const pool = require("./mysqlPool").pool;
+const {Type} = require("./type");
+
 const NAME_MIN = 5;
 exports.NAME_MIN = NAME_MIN;
 const NAME_MAX = 50;
 exports.NAME_MAX = NAME_MAX;
 const CREDITS_MIN = 32;
 exports.CREDITS_MIN = CREDITS_MIN;
-const pool = require("./mysqlPool").pool;
+
+// Validates an object against a provided schema.
+//
+// Note:
+// - An object can have properties that are not in the schema and can still
+//   be valid. Extra properties not in schema are ignored in sanitization.
+//
+// - For request bodies coming from PATCH requests, not all required properties
+//   are provided. In these cases, this function checks for "some" required
+//   keys instead of "every" key against the schema.
+function validateAgainstSchema(obj, schema, isPatch = false) {
+  if (isPatch) {
+    // object is valid if it exists and there must exist a property in object
+    // that is also in the schema and is valid within that schema
+    return obj && Object.keys(obj).some(key => {
+      hasProperty(schema, key) && validateProperty(obj, key, schema);
+    });
+  } else {
+    // object is valid if it exists and for every required property in schema,
+    // the object must also have that key
+    return obj && Object.keys(schema).every(key => {
+      // a property is valid if it's not required by the schema,
+      // otherwise it must pass the validation
+      !schema[key].required || validateProperty(obj, key, schema);
+    });
+  }
+}
+exports.validateAgainstSchema = validateAgainstSchema;
+
+// Validates a single property of an object against a provided schema.
+//
+// The property should be already in the schema, but there's a fail-safe that
+// returns false if the schema doesn't have the property.
+function validateProperty(obj, property, schema) {
+  // fail-safe to avoid TypeError when reading property of `undefined`
+  if (!hasProperty(schema, property)) {
+    return false;
+  }
+
+  switch (schema[property].type) {
+    case Type.integer:
+      return validator.isInt(obj[property] + "", {
+        min: schema[property].minValue,
+        max: schema[property].maxValue
+      });
+
+    case Type.string:
+      return validator.isLength(obj[property] + "", {
+        min: schema[property].minLength,
+        max: schema[property].maxLength
+      });
+
+    case Type.timestamp:
+      // time format must be ISO 8601, e.g. "2020-05-15T14:30:29Z"
+      return validator.isISO8601(obj[property] + "", {
+        strict: true
+      });
+    default:
+      return false;
+  }
+}
+
+// Sanitizes an object using a provided schema by extracting valid properties to
+// a new object and returns that object.
+//
+// Note:
+// - This function assumes the object has been validated against the schema.
+// - This function does not support nested objects or with arrays inside.
+function sanitizeObject(obj, schema) {
+  const sanitizedObj = {};
+  if (obj) {
+    Object.keys(schema).forEach(key => {
+      if (hasProperty(obj, key)) {
+        sanitizedObj[key] = obj[key];
+      }
+    });
+  }
+  return sanitizedObj;
+}
+exports.sanitizeObject = sanitizeObject;
 
 // checks that the submitted form data does not violate any constraints
 // returns a value that can be used to identify which constraint was violated
@@ -253,4 +336,10 @@ function creditConstraint(userId, planName, courses) {
 
   });
 
+}
+
+// A shorthand to check whether an object has a property that is
+// neither `null` nor `undefined`.
+function hasProperty(obj, property) {
+  return obj && obj[property] !== null && obj[property] !== undefined;
 }
