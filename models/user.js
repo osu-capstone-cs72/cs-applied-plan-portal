5e696093e27b1b0b1e22a5969eed9774e0fbfb48
silverware13 <thomasza@oregonstate.edu>
2020-01-27 12:44:55 -0800

    Add getUserPlans function, which lists all plans that a user has created

--- models/user.js
+++ models/user.js
@@ -1,2 +1,31 @@
 // File: user.js
 // Description: data functions that handle users
+
+const pool = require("../utils/mysqlPool").pool;
+
+// search for a list of the plans that a user has created
+function getUserPlans(userId) {
+
+  return new Promise((resolve, reject) => {
+
+    const sql = "SELECT * FROM Plan WHERE studentId = ?;";
+    pool.query(sql, [userId], (err, results) => {
+
+      if (err) {
+        console.log("Error searching for user plans");
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+
+  })
+    .then((results) => {
+      return results;
+    })
+    .catch((err) => {
+      throw Error(err);
+    });
+
+}
+exports.getUserPlans = getUserPlans;
