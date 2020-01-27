dd5308111733e406df9042b13740a5a9630e08b9
silverware13 <thomasza@oregonstate.edu>
2020-01-27 12:47:53 -0800

    Add front-end example for getting a list of plans using a user ID

---
+++ test_example/public/planList.js
@@ -0,0 +1,50 @@
+console.log("JavaScript start");
+
+// define constant for the "Submit Plan" button
+const submitButton = document.getElementById("submit-button");
+
+// define variables for the input text values
+let courseCode = "";
+
+// assign all of the form text values to variables
+function getInputs() {
+  courseCode = document.getElementById("course-code").value;
+}
+
+// clear all of the input text fields of the form
+function clearInputs() {
+  document.getElementById("plan-form").reset();
+}
+
+// submit the users plan
+function submitPlan(courseCode) {
+
+  const postRequest = new XMLHttpRequest();
+  const postURL = `/user/${courseCode}/plan`;
+  postRequest.open("GET", postURL);
+  console.log(postURL);
+
+  postRequest.setRequestHeader("Content-Type", "application/json");
+
+  postRequest.addEventListener("load", (event) => {
+    if (event.target.status !== 200) {
+      alert("Error submitting plan:\n" + event.target.response);
+    } else {
+      alert("Response:\n" + event.target.response);
+
+      // turn the result into an object
+      const obj = JSON.parse(event.target.response);
+
+      clearInputs();
+    }
+  });
+
+  postRequest.send();
+}
+
+// user clicks on the "Submit Plan" button
+submitButton.addEventListener("click", () => {
+  console.log("Submit button pressed");
+  getInputs();
+  submitPlan(courseCode);
+});
