f02c31c1cc419ff4fad9d1d3b8600f4e6094bc6a
silverware13 <thomasza@oregonstate.edu>
2020-01-27 14:48:45 -0800

    Add getComment function

--- models/comment.js
+++ models/comment.js
@@ -1,2 +1,31 @@
 // File: comment.js
 // Description: data functions that handle comments
+
+const pool = require("../utils/mysqlPool").pool;
+
+// get a comment by its ID
+function getComment(commentId) {
+
+  return new Promise((resolve, reject) => {
+
+    const sql = "SELECT * FROM Comment WHERE commentId = ?;";
+    pool.query(sql, [commentId], (err, results) => {
+
+      if (err) {
+        console.log("Error searching for comment");
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    });
+
+  })
+    .then((results) => {
+      return results;
+    })
+    .catch((err) => {
+      throw Error(err);
+    });
+
+}
+exports.getComment = getComment;
