7e2fb8ac52c00d6afcbc4531e1fd0a27b4bd73a3
Phi Luu <philectron@users.noreply.github.com>
2020-01-27 21:05:36 -0800

    Add validation error message in schema

--- models/plan.js
+++ models/plan.js
@@ -10,23 +10,42 @@ const planSchema = {
     required: true,
     type: Type.integer,
     minValue: 0,
-    maxValue: 4
+    maxValue: 4,
+    getErrorMessage: function() {
+      return "Constraint violated: Invalid plan status\n" +
+        "Plan status must be 0 (Rejected), 1 (Awaiting Student Changes) " +
+        "2 (Awaiting Reivew), 3 (Awaiting Final Review), or 4 (Accepted).";
+    }
   },
   planName: {
     required: true,
     type: Type.string,
     minLength: 5,
-    maxLength: 50
+    maxLength: 50,
+    getErrorMessage: function() {
+      return "Constraint violated: Invalid plan name\n" +
+        `The plan name must be a string between ${this.minLength} and ` +
+        `${this.maxLength} characters long.`;
+    }
   },
   studentId: {
     required: true,
     type: Type.integer,
     minValue: 1,
-    maxValue: Infinity
+    maxValue: Infinity,
+    getErrorMessage: function() {
+      return "Constraint violated: Invalid user ID\n" +
+        "The user ID associated with this plan must be an integer at least " +
+        `${this.minValue}.`;
+    }
   },
   lastUpdated: {
     required: true,
-    type: Type.timestamp
+    type: Type.timestamp,
+    getErrorMessage: function() {
+      return "Constraint violated: Invalid plan timestamp\n" +
+        "The plan timestamp must be in ISO 8601 format.";
+    }
   }
 };
 exports.planSchema = planSchema;
