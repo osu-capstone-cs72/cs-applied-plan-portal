11273ef22459dcf982cf5238df0107c31b3692ab
silverware13 <thomasza@oregonstate.edu>
2020-01-29 13:35:10 -0800

    Update comments to use async/await and mysql2

--- api/comment.js
+++ api/comment.js
@@ -9,45 +9,47 @@ const getComment = require("../models/comment").getComment;
 const createComment = require("../models/comment").createComment;
 
 // get comment by id
-app.get("/:commentId", (req, res) => {
+app.get("/:commentId", async (req, res) => {
 
   const commentId = req.params.commentId;
   console.log("Get comment", commentId);
 
-  getComment(commentId)
-    .then((results) => {
-      if (results.length === 0) {
-        console.log("No comment found - 404\n");
-        res.status(404).send("No comment found.");
-      } else {
-        console.log("Comment found - 200\n");
-        res.status(200).send(results);
-      }
-    })
-    .catch((err) => {
-      console.log("An internal server error occurred - 500\n Error:", err);
-      res.status(500).send("An internal server error occurred. Please try again later.");
-    });
+  try {
+
+    const results = await getComment(commentId);
+    if (results.length === 0) {
+      console.log("No comment found - 404\n");
+      res.status(404).send("No comment found.");
+    } else {
+      console.log("Comment found - 200\n");
+      res.status(200).send(results);
+    }
+
+  } catch (err) {
+    console.log("An internal server error occurred - 500\n Error:", err);
+    res.status(500).send("An internal server error occurred. Please try again later.");
+  }
 
 });
 
 // create a new comment
-app.post("/:planId/:userId/:text", (req, res) => {
+app.post("/:planId/:userId/:text", async (req, res) => {
 
   const planId = req.params.planId;
   const userId = req.params.userId;
   const text = req.params.text;
   console.log("User ", userId, "creating a comment on plan", planId);
 
-  createComment(planId, userId, text)
-    .then(() => {
-      console.log("Comment created - 201\n");
-      res.status(201).send("Comment created");
-    })
-    .catch((err) => {
-      console.log("An internal server error occurred - 500\n Error:", err);
-      res.status(500).send("An internal server error occurred. Please try again later.");
-    });
+  try {
+
+    await createComment(planId, userId, text);
+    console.log("Comment created - 201\n");
+    res.status(201).send("Comment created");
+
+  } catch (err) {
+    console.log("An internal server error occurred - 500\n Error:", err);
+    res.status(500).send("An internal server error occurred. Please try again later.");
+  }
 
 });
 
--- models/comment.js
+++ models/comment.js
@@ -4,49 +4,35 @@
 const pool = require("../utils/mysqlPool").pool;
 
 // get a comment by its ID
-function getComment(commentId) {
+async function getComment(commentId) {
 
-  return new Promise((resolve, reject) => {
+  try {
 
     const sql = "SELECT * FROM Comment WHERE commentId = ?;";
-    pool.query(sql, [commentId], (err, results) => {
+    const results = await pool.query(sql, [commentId]);
+    return results[0];
 
-      if (err) {
-        console.log("Error searching for comment");
-        reject(err);
-      } else {
-        resolve(results);
-      }
-    });
-
-  })
-    .catch((err) => {
-      throw Error(err);
-    });
+  } catch (err) {
+    console.log("Error searching for comment");
+    throw Error(err);
+  }
 
 }
 exports.getComment = getComment;
 
 // create a new comment by its ID
-function createComment(planId, userId, text) {
+async function createComment(planId, userId, text) {
 
-  return new Promise((resolve, reject) => {
+  try {
 
     const sql = "INSERT INTO Comment (planId, userId, text) VALUES (?, ?, ?);;";
-    pool.query(sql, [planId, userId, text], (err) => {
+    const results = await pool.query(sql, [planId, userId, text]);
+    return results[0];
 
-      if (err) {
-        console.log("Error adding comment");
-        reject(err);
-      } else {
-        resolve();
-      }
-    });
-
-  })
-    .catch((err) => {
-      throw Error(err);
-    });
+  } catch (err) {
+    console.log("Error adding comment");
+    throw Error(err);
+  }
 
 }
 exports.createComment = createComment;
