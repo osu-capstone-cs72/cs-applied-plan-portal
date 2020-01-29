a699e53d545834fa2b4c88ec7b8850d3e953eae2
silverware13 <thomasza@oregonstate.edu>
2020-01-29 13:06:19 -0800

    Update users to use async/await and mysql2

--- api/user.js
+++ api/user.js
@@ -8,25 +8,26 @@ const app = express();
 const getUserPlans = require("../models/user").getUserPlans;
 
 // get all of the plans created by user
-app.get("/:userId/plan", (req, res) => {
+app.get("/:userId/plan", async (req, res) => {
 
   const userId = req.params.userId;
   console.log("Get all of the plans created by user", userId);
 
-  getUserPlans(userId)
-    .then((results) => {
-      if (results.length === 0) {
-        console.log("No plans found - 404\n");
-        res.status(404).send("No plans found.");
-      } else {
-        console.log("Plans found - 200\n");
-        res.status(200).send(results);
-      }
-    })
-    .catch((err) => {
-      console.log("An internal server error occurred - 500\n Error:", err);
-      res.status(500).send("An internal server error occurred. Please try again later.");
-    });
+  try {
+
+    const results = await getUserPlans(userId);
+    if (results.length === 0) {
+      console.log("No plans found - 404\n");
+      res.status(404).send("No plans found.");
+    } else {
+      console.log("Plans found - 200\n");
+      res.status(200).send(results);
+    }
+
+  } catch (err) {
+    console.log("An internal server error occurred - 500\n Error:", err);
+    res.status(500).send("An internal server error occurred. Please try again later.");
+  }
 
 });
 
--- models/user.js
+++ models/user.js
@@ -4,28 +4,16 @@
 const pool = require("../utils/mysqlPool").pool;
 
 // search for a list of the plans that a user has created
-function getUserPlans(userId) {
+async function getUserPlans(userId) {
 
-  return new Promise((resolve, reject) => {
-
+  try {
     const sql = "SELECT * FROM Plan WHERE studentId = ?;";
-    pool.query(sql, [userId], (err, results) => {
-
-      if (err) {
-        console.log("Error searching for user plans");
-        reject(err);
-      } else {
-        resolve(results);
-      }
-    });
-
-  })
-    .then((results) => {
-      return results;
-    })
-    .catch((err) => {
-      throw Error(err);
-    });
+    const results = await pool.query(sql, [userId]);
+    return results[0];
+  } catch (err) {
+    console.log("Error searching for user plans");
+    throw Error(err);
+  }
 
 }
 exports.getUserPlans = getUserPlans;
