799ba1329f8f509bcc31f7c9c092a86807d4abf2
silverware13 <thomasza@oregonstate.edu>
2020-01-29 11:13:26 -0800

    Add API endpoint for POST comment

--- api/comment.js
+++ api/comment.js
@@ -6,6 +6,7 @@ const express = require("express");
 const app = express();
 
 const getComment = require("../models/comment").getComment;
+const createComment = require("../models/comment").createComment;
 
 // get comment by id
 app.get("/:commentId", (req, res) => {
@@ -22,6 +23,26 @@ app.get("/:commentId", (req, res) => {
         console.log("Comment found - 200\n");
         res.status(200).send(results);
       }
+    })
+    .catch((err) => {
+      console.log("An internal server error occurred - 500\n Error:", err);
+      res.status(500).send("An internal server error occurred. Please try again later.");
+    });
+
+});
+
+// create a new comment
+app.post("/:planId/:userId/:text", (req, res) => {
+
+  const planId = req.params.planId;
+  const userId = req.params.userId;
+  const text = req.params.text;
+  console.log("User ", userId, "creating a comment on plan", planId);
+
+  createComment(planId, userId, text)
+    .then(() => {
+      console.log("Comment created - 201\n");
+      res.status(201).send("Comment created");
     })
     .catch((err) => {
       console.log("An internal server error occurred - 500\n Error:", err);
