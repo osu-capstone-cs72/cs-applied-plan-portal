49c09803b7bf3c34867191b10835001e8a1cf30a
silverware13 <thomasza@oregonstate.edu>
2020-01-31 14:11:02 -0800

    Set for new database

--- other/test_db.sql
+++ other/test_db.sql
@@ -19,7 +19,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `sql3317654`
+-- Database: `testdatabase_osu`
 --
 
 -- --------------------------------------------------------
