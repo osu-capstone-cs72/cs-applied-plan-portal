433b192faf7dcedc9f3b2f413f6f22178da77483
silverware13 <thomasza@oregonstate.edu>
2020-02-03 09:17:15 -0800

    Add React router and integrate example

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React from "react";
+import {Route, Switch} from "react-router-dom";
 import StudentCreatePlan from "./StudentCreatePlan";
+import List from "./List";
+import Home from "./Home";
 
+// <StudentCreatePlan />
 function App() {
   return (
     <div className="App">
-      <StudentCreatePlan />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/list">
+          <List />
+        </Route>
+      </Switch>
+
     </div>
   );
 }
