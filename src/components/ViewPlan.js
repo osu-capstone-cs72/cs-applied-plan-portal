26d26f87f779b43b4662bd2329922b2a9d17444f
silverware13 <thomasza@oregonstate.edu>
2020-02-03 13:58:50 -0800

    Add the starting point of the ViewPlan component

---
+++ src/components/ViewPlan.js
@@ -0,0 +1,14 @@
+import React from "react";
+import EditPlan from "./EditPlan";
+import CourseContainer from "./CourseContainer";
+
+export default class ViewPlan extends React.Component {
+
+  render() {
+    return (
+      <div className="view-plan">
+        <p>Welcome to the view plan page</p>
+      </div>
+    );
+  }
+}
