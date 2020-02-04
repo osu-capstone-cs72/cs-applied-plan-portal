49185be541f80cefd0e9d712ab8b4ee26d1c700c
Phi Luu <philectron@users.noreply.github.com>
2020-02-04 03:58:54 -0800

    Add GET /api/user/{onid} API route
    I'm planning to use Morgan to log the request more effectively, rather
    than manually logging them using JS console.

--- models/user.js
+++ models/user.js
@@ -1,19 +1,29 @@
 // File: user.js
-// Description: data functions that handle users
+// Description: Provides data functions that handle the User entity.
 
 const pool = require("../utils/mysqlPool").pool;
 
+async function getUserByOnid(onid) {
+  try {
+    const sql = "SELECT * FROM User WHERE userId = ?";
+    const results = await pool.query(sql, [onid]);
+    return results[0];
+  } catch (err) {
+    console.error("Error fetching User");
+    throw Error(err);  // bubble up the error
+  }
+}
+exports.getUserByOnid = getUserByOnid;
+
 // search for a list of the plans that a user has created
 async function getUserPlans(userId) {
-
   try {
-    const sql = "SELECT * FROM Plan WHERE studentId = ?;";
+    const sql = "SELECT * FROM Plan WHERE studentId = ?";
     const results = await pool.query(sql, [userId]);
     return results[0];
   } catch (err) {
-    console.log("Error searching for user plans");
+    console.error("Error searching for user plans");
     throw Error(err);
   }
-
 }
 exports.getUserPlans = getUserPlans;
