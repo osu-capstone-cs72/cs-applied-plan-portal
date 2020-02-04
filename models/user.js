574d70e1d1fbd88853be3caf12eca4a7f68ebd11
Phi Luu <philectron@users.noreply.github.com>
2020-02-04 04:07:15 -0800

    Make some small changes to getUserPlans in models

--- models/user.js
+++ models/user.js
@@ -10,20 +10,20 @@ async function getUserByOnid(onid) {
     return results[0];
   } catch (err) {
     console.error("Error fetching User");
-    throw Error(err);  // bubble up the error
+    throw Error(err);  // bubble the error up
   }
 }
 exports.getUserByOnid = getUserByOnid;
 
 // search for a list of the plans that a user has created
-async function getUserPlans(userId) {
+async function getUserPlans(onid) {
   try {
     const sql = "SELECT * FROM Plan WHERE studentId = ?";
-    const results = await pool.query(sql, [userId]);
+    const results = await pool.query(sql, [onid]);
     return results[0];
   } catch (err) {
-    console.error("Error searching for user plans");
-    throw Error(err);
+    console.error("Error fetching User's Plans");
+    throw Error(err);  // bubble the error up
   }
 }
 exports.getUserPlans = getUserPlans;
