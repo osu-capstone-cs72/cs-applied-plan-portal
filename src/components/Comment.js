d843c82aae85b738bcb2f906b0c9213693b3c744
silverware13 <thomasza@oregonstate.edu>
2020-02-04 02:24:14 -0800

    Have comment components show user, time, and text

--- src/components/Comment.js
+++ src/components/Comment.js
@@ -4,8 +4,10 @@ export default class Comment extends React.Component {
 
   render() {
     return (
-      <div className="comment">
-        <p>I am a comment</p>
+      <div className="comment-container">
+        <p className="comment-user">Han Solo</p>
+        <p className="comment-time">2/4/2020 5pm</p>
+        <p className="comment-text">This is a comment</p>
       </div>
     );
   }
