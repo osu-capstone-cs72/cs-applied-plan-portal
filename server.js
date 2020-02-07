f2f16ae508c982541d28431457602f2540f8dfa9
silverware13 <thomasza@oregonstate.edu>
2020-02-07 08:52:08 -0800

    Set API server to make multiple attempts to connect to the database before failing

--- server.js
+++ server.js
@@ -9,19 +9,24 @@ const pool = require("./utils/mysqlPool").pool;
 const app = require("./api/index");
 
 // confirm that connection was made to the database
-async function testConnection(pool, callback) {
+async function testConnection(pool, attempt, callback) {
   try {
     await pool.query("SELECT courseId FROM Course WHERE courseId = 1");
     console.log("Connected to database");
     callback();
   } catch (err) {
-    console.log("Error connecting to database \n", err);
+    if (attempt < 5) {
+      console.log(`Attempt ${attempt}: Error connecting to database ... Restarting ...`);
+      testConnection(pool, attempt + 1, callback);
+    } else {
+      console.log(`Final Attempt: Error connecting to database\n`, err);
+    }
   }
 }
 
 // listen for incoming requests
 const port = process.env.PORT || 5000;
-testConnection(pool, () => {
+testConnection(pool, 1, () => {
   app.listen(port, () => {
     console.log("Server is listening on port", port, "\n");
   });
