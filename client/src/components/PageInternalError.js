8044166fa5eb5a5153dc812477ec54d22010b500
silverware13 <thomasza@oregonstate.edu>
2020-02-08 09:57:20 -0800

    Use emotion for error page components

--- client/src/components/PageInternalError.js
+++ client/src/components/PageInternalError.js
@@ -1,11 +1,24 @@
-import React from "react";
+/** @jsx jsx */
+
+import {css, jsx} from "@emotion/core";
 
 function PageInternalError() {
+
+  const styleStatusContainer = css`
+    margin: 100px;
+  `;
+
+  const styleStatusCode = css`
+  `;
+
+  const styleStatusText = css`
+  `;
+
   return (
 
-    <div className="status-container">
-      <h1 className="status-code">500 Internal Server Error</h1>
-      <p className="status-text">The server encountered an internal error
+    <div className="status-container" css={styleStatusContainer}>
+      <h1 className="status-code" css={styleStatusCode}>500 Internal Server Error</h1>
+      <p className="status-text" css={styleStatusText}>The server encountered an internal error
             and was unable to complete your request.</p>
     </div>
 
--- client/src/components/PageNotFound.js
+++ client/src/components/PageNotFound.js
@@ -1,11 +1,24 @@
-import React from "react";
+/** @jsx jsx */
+
+import {css, jsx} from "@emotion/core";
 
 function PageNotFound() {
+
+  const styleStatusContainer = css`
+    margin: 100px;
+  `;
+
+  const styleStatusCode = css`
+  `;
+
+  const styleStatusText = css`
+  `;
+
   return (
 
-    <div className="status-container">
-      <h1 className="status-code">404 Page Not Found</h1>
-      <p className="status-text">The page you are looking for does not exist.</p>
+    <div className="status-container" css={styleStatusContainer}>
+      <h1 className="status-code" css={styleStatusCode}>404 Page Not Found</h1>
+      <p className="status-text" css={styleStatusText}>The page you are looking for does not exist.</p>
     </div>
 
   );
