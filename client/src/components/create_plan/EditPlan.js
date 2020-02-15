1a62fb625eca74467c8b7c0f16b102b80fc2091f
silverware13 <thomasza@oregonstate.edu>
2020-02-15 15:17:56 -0800

    Reduce the amount of code impacted by changes

--- client/src/components/create_plan/EditPlan.js
+++ client/src/components/create_plan/EditPlan.js
@@ -199,9 +199,8 @@ export default class EditPlan extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {this.state.courses.map(c => <PlanCourse key={c.courseId} id={c.courseId}
-              courseCode={c.courseCode} courseName={c.courseName} credits={c.credits}
-              remove={this.removeCourse}/>)}
+            {this.state.courses.map(c => <PlanCourse key={c.courseId} courseCode={c.courseCode}
+              courseName={c.courseName} credits={c.credits} remove={this.removeCourse}/>)}
           </tbody>
         </table>
         <button className="submit-button" onClick={this.submitPlan}>Submit</button>
