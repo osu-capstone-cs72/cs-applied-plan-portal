d62d0ba0ddbc6937c559e3e97a8372134947889c
silverware13 <thomasza@oregonstate.edu>
2020-02-16 13:59:43 -0800

    API create review request returns time

--- models/review.js
+++ models/review.js
@@ -8,11 +8,21 @@ async function createReview(planId, advisorId, newStatus) {
 
   try {
 
-    const sql = "BEGIN;" +
+    let sql = "BEGIN;" +
     "INSERT INTO PlanReview (planId, advisorId, newStatus) VALUES (?, ?, ?); " +
     "UPDATE Plan SET status=? WHERE planId=?; COMMIT;";
-    const results = await pool.query(sql, [planId, advisorId, newStatus, newStatus, planId]);
-    return {insertId: results[0][1].insertId};
+    let results = await pool.query(sql, [planId, advisorId, newStatus, newStatus, planId]);
+    const reviewId = results[0][1].insertId;
+
+    sql = "SELECT timeReviewed FROM PlanReview WHERE reviewId=?;";
+    results = await pool.query(sql, [reviewId]);
+
+    const obj = {
+      insertId: reviewId,
+      time: results[0][0].timeReviewed
+    };
+
+    return obj;
 
   } catch (err) {
     console.log("Error adding review");
