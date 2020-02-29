196b96c9a9c0f6ca041a3be65a3ca6687b5da4c2
silverware13 <thomasza@oregonstate.edu>
2020-02-29 09:08:36 -0800

    Add search options to advisor home page

--- client/src/components/AdvisorHome.js
+++ client/src/components/AdvisorHome.js
@@ -23,9 +23,11 @@ function AdvisorHome(props) {
       width: 50%;
     }
 
-    #plan-selection-container {
+    #plan-search-container {
       position: relative;
       top: 75px;
+      padding: 10px;
+      border: 1px solid black;
     }
 
     .home-error-message-container {
@@ -36,6 +38,31 @@ function AdvisorHome(props) {
     .advisor-plans-table {
       position: relative;
       top: 100px;
+    }
+
+    #plan-search-container {
+      margin: 0 auto;
+      width: 70%;
+    }
+
+    #input-search {
+      width: 90%;
+    }
+
+    #search-plan-button {
+      width: 10%;
+    }
+
+    #filter-container {
+      display: inline-block;
+      vertical-align: top;
+      padding: 10px;
+      width: 100%;
+    }
+
+    .advisor-plan-filter {
+      display:inline-block;
+      width: 100%;
     }
 
   `;
@@ -97,7 +124,7 @@ function AdvisorHome(props) {
       case 4:
         return "Accepted";
       default:
-        return "Undefined status";
+        return "";
     }
   }
 
@@ -111,27 +138,49 @@ function AdvisorHome(props) {
         <PageSpinner loading={loading} />
         <NavBar />
         <div id="plan-data-container">
-          <div id="plan-selection-container">
-            <select id="select-status" className="advisor-plan-select">
-              <option value="5">Any</option>
-              <option value="2">Awaiting Review</option>
-              <option value="3">Awaiting final review</option>
-              <option value="1">Awaiting student changes</option>
-              <option value="4">Accepted</option>
-              <option value="0">Rejected</option>
-            </select>
-            <select id="select-time" className="advisor-plan-select">
-              <option value="1">Time Created</option>
-              <option value="0">Time Updated</option>
-            </select>
-            <select id="select-order" className="advisor-plan-select">
-              <option value="1">Ascending</option>
-              <option value="0">Decending</option>
-            </select>
-            <button id="search-plan-status-button" onClick={() => { fetchPlans(); }}>
-              Search
-            </button>
+
+          <div id="plan-search-container">
+            <div id="plan-search-bar">
+              <input type="text" id="input-search" />
+              <button id="search-plan-button" onClick={() => { fetchPlans(); }}>
+                Search
+              </button>
+            </div>
+
+            <div id="filter-container">
+              <label className="filter-label">Search By</label>
+              <select id="select-status" className="advisor-plan-filter">
+                <option value="0" selected>User Name</option>
+                <option value="1">User ID</option>
+                <option value="2">Plan Name</option>
+              </select>
+              <label className="filter-label">Status</label>
+              <select id="select-status" className="advisor-plan-filter">
+                <option value="5" selected>Any</option>
+                <option value="2">Awaiting Review</option>
+                <option value="3">Awaiting final review</option>
+                <option value="1">Awaiting student changes</option>
+                <option value="4">Accepted</option>
+                <option value="0">Rejected</option>
+              </select>
+              <label className="filter-label">Sort By</label>
+              <select id="select-time" className="advisor-plan-filter">
+                <option value="0">User Name</option>
+                <option value="1">User ID</option>
+                <option value="2">Plan Name</option>
+                <option value="3">Status</option>
+                <option value="4">Time Created</option>
+                <option value="5" selected>Time Updated</option>
+              </select>
+              <label className="filter-label">Order</label>
+              <select id="select-order" className="advisor-plan-filter">
+                <option value="1" selected>Ascending</option>
+                <option value="0">Decending</option>
+              </select>
+            </div>
+
           </div>
+
           <div className="home-error-message-container">{errorMessage}</div>
           <table className="advisor-plans-table">
             <tbody>
@@ -140,8 +189,8 @@ function AdvisorHome(props) {
                 <th className="student-plans-data">User ID</th>
                 <th className="student-plans-data">Plan Name</th>
                 <th className="student-plans-data">Status</th>
-                <th className="student-plans-data">Created</th>
-                <th className="student-plans-data">Updated</th>
+                <th className="student-plans-data">Time Created</th>
+                <th className="student-plans-data">Time Updated</th>
               </tr>
               {plans ? plans.map(plan =>
                 <tr key={plan.planId} onClick={() => goToPlan(plan)}>
