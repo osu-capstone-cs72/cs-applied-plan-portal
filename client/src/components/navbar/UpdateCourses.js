4ffb4d0660c767cebed6d44755af90cca4c95d96
silverware13 <thomasza@oregonstate.edu>
2020-03-28 01:37:22 -0700

    Add confirmation message for updating courses

--- client/src/components/navbar/UpdateCourses.js
+++ client/src/components/navbar/UpdateCourses.js
@@ -1,6 +1,7 @@
 /** @jsx jsx */
 
 import {css, jsx} from "@emotion/core";
+import {getToken} from "../../utils/authService";
 
 function UpdateCourses() {
 
@@ -23,8 +24,39 @@ function UpdateCourses() {
 
   // update the available courses in the database
   // using the OSU course catalog
-  function startCourseUpdate() {
-    // logout();
+  async function startCourseUpdate() {
+
+    const firstPrompt = "Update the list of available courses to match the " +
+      "current OSU course catalog?";
+    const secondPrompt = "Updating the courses may take anywhere from a few minutes " +
+      "to a few hours.\n\n " +
+      "This action should only be performed when there is not " +
+      "a large amount of user traffic and only when there is the expectation " +
+      "of new courses existing in the OSU course catalog (once a term).\n\n" +
+      "Do you still want to update the current list of available courses?";
+
+    if (window.confirm(firstPrompt) && window.confirm(secondPrompt)) {
+
+      try {
+
+        // request that all courses are updated
+        const token = getToken();
+        const server = `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}`;
+        const url = `http://${server}/course/updateDatabase?accessToken=${token}`;
+
+        // perform the query
+        const response = await fetch(url);
+        if (!response.ok) {
+          // note that there was an error trying to start the update
+          alert("Internal server error. Unable to update courses.");
+        }
+
+      } catch (err) {
+        // note that there was an error trying to start the update
+        alert("Internal server error. Unable to update courses.");
+      }
+    }
+
   }
 
   return (
