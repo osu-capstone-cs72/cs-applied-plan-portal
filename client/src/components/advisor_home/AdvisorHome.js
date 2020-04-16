ec593a7cdcddc7507249bd2b07140028887a0799
silverware13 <thomasza@oregonstate.edu>
2020-04-16 01:10:51 -0700

    Add abort controller to advisor homepage

--- client/src/components/advisor_home/AdvisorHome.js
+++ client/src/components/advisor_home/AdvisorHome.js
@@ -43,6 +43,99 @@ function AdvisorHome() {
   // if the sorting order changes perform a new search
   useEffect(() => {
 
+    // set ignore and controller to prevent a memory leak
+    // in the case where we need to abort early
+    let ignore = false;
+    const controller = new AbortController();
+
+    // search for plans using various sorting logic
+    async function searchPlans(cursor, newSearch) {
+      try {
+        setErrorMessage("");
+        setLoading(true);
+        const sortValue = searchFields.sortValue;
+        const orderValue = searchFields.orderValue;
+
+        // get search text from searchbar
+        const text = document.getElementById("input-search");
+        let textValue = text.value;
+
+        // if search text is empty we use a special char to represent
+        // any text response as valid
+        if (textValue === "") {
+          textValue = "*";
+        }
+
+        // get the status from the status select
+        const status = document.getElementById("select-status");
+        let statusValue = status.options[status.selectedIndex].value;
+
+        // only set the search values if we are performing a new search
+        if (newSearch) {
+          setSearchFields({
+            textValue: textValue,
+            statusValue: statusValue,
+            sortValue: sortValue,
+            orderValue: orderValue
+          });
+        } else {
+          textValue = searchFields.textValue;
+          statusValue = searchFields.statusValue;
+        }
+
+        // construct the request url
+        const getUrl = `/api/plan/search/${textValue}/${statusValue}/` +
+          `${sortValue}/${orderValue}/${cursor.primary}/${cursor.secondary}`;
+        let obj = {};
+
+        // get our search results
+        const results = await fetch(getUrl);
+
+        // before checking the results, ensure the request was not canceled
+        if (!ignore) {
+
+          if (results.ok) {
+
+            // if the cursor is new then we will want to relist plans
+            obj = await results.json();
+            if (cursor.primary === "null") {
+              setPlans([...obj.plans]);
+            } else {
+              setPlans([...plans, ...obj.plans]);
+            }
+            setCursor(obj.nextCursor);
+
+          } else {
+            // we got a bad status code. Show the error
+            obj = await results.json();
+            setErrorMessage(obj.error);
+            if (results.status === 403) {
+              // if the user is not allowed to search plans,
+              // redirect to login to allow updating of user info
+              login();
+            }
+            if (results.status === 500) {
+              setErrorMessage("An internal server error occurred. Please try again later.");
+            }
+            setPlans([]);
+          }
+
+          setLoading(false);
+
+        }
+
+      } catch (err) {
+        if (err instanceof DOMException) {
+          // if we canceled the fetch request then don't show an error message
+          console.log("HTTP request aborted");
+        } else {
+          // show error message if error while searching
+          setErrorMessage("An internal server error occurred. Please try again later.");
+        }
+      }
+
+    }
+
     // don't load search results on the initial mount
     if (mounted) {
       const newCursor = {
@@ -54,82 +147,14 @@ function AdvisorHome() {
       setMounted(true);
     }
 
+    // cleanup function
+    return () => {
+      controller.abort();
+      ignore = true;
+    };
+
     // eslint-disable-next-line
   }, [searchFields.orderValue, searchFields.sortValue]);
-
-  // search for plans using various sorting logic
-  async function searchPlans(cursor, newSearch) {
-    try {
-      setErrorMessage("");
-      setLoading(true);
-      const sortValue = searchFields.sortValue;
-      const orderValue = searchFields.orderValue;
-
-      // get search text from searchbar
-      const text = document.getElementById("input-search");
-      let textValue = text.value;
-
-      // if search text is empty we use a special char to represent
-      // any text response as valid
-      if (textValue === "") {
-        textValue = "*";
-      }
-
-      // get the status from the status select
-      const status = document.getElementById("select-status");
-      let statusValue = status.options[status.selectedIndex].value;
-
-      // only set the search values if we are performing a new search
-      if (newSearch) {
-        setSearchFields({
-          textValue: textValue,
-          statusValue: statusValue,
-          sortValue: sortValue,
-          orderValue: orderValue
-        });
-      } else {
-        textValue = searchFields.textValue;
-        statusValue = searchFields.statusValue;
-      }
-
-      // construct the request url
-      const getUrl = `/api/plan/search/${textValue}/${statusValue}/` +
-        `${sortValue}/${orderValue}/${cursor.primary}/${cursor.secondary}`;
-      let obj = {};
-
-      // get our search results
-      const results = await fetch(getUrl);
-      if (results.ok) {
-
-        // if the cursor is new then we will want to relist plans
-        obj = await results.json();
-        if (cursor.primary === "null") {
-          setPlans([...obj.plans]);
-        } else {
-          setPlans([...plans, ...obj.plans]);
-        }
-        setCursor(obj.nextCursor);
-
-      } else {
-        // we got a bad status code. Show the error
-        obj = await results.json();
-        setErrorMessage(obj.error);
-        if (results.status === 403) {
-          // if the user is not allowed to search plans,
-          // redirect to login to allow updating of user info
-          login();
-        }
-        if (results.status === 500) {
-          setErrorMessage("An internal server error occurred. Please try again later.");
-        }
-        setPlans([]);
-      }
-    } catch (err) {
-      // show error message if error while searching
-      setErrorMessage("An internal server error occurred. Please try again later.");
-    }
-    setLoading(false);
-  }
 
   // update the sorting rules
   function handleChangeSort(sort, order) {
