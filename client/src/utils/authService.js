e3e94f944a38faee7416dfe49347dc0d1844c2a4
silverware13 <thomasza@oregonstate.edu>
2020-05-08 15:17:54 -0700

    Test with hard coded user

--- client/src/utils/authService.js
+++ client/src/utils/authService.js
@@ -8,46 +8,10 @@ import validator from "validator";
 // On error returns a student with a user ID of 0.
 export function getProfile() {
   return {
-    userId: process.env.REACT_APP_TEST_NO_AUTH_USERID,
+    userId: 82757579527,
     role: 0,
   };
 
-  try {
-    // parse the cookie included in document; must string-coerce it because
-    // cookie.parse() throws on non-string arguments
-    const cookieObj = cookie.parse(`${document.cookie}`);
-
-    // ensure the parsed cookies are a JS object (it should always be)
-    if (cookieObj !== Object(cookieObj)) {
-      throw new Error("Cookies are not a valid JS object");
-    }
-
-    // ensure that the userId and role cookies exist
-    if (!cookieObj.userId || !cookieObj.role) {
-      throw new Error("User ID or role cookie not set");
-    }
-
-    // ensure userId and role are non-negative integers
-    if (
-      !validator.isInt(cookieObj.userId + "") ||
-      !validator.isInt(cookieObj.role + "")
-    ) {
-      throw new Error("Negative user ID or role");
-    }
-
-    // success case; return an object containing userId and role in integers
-    return {
-      userId: validator.toInt(cookieObj.userId),
-      role: validator.toInt(cookieObj.role),
-    };
-  } catch (err) {
-    // return an invalid user
-    console.error(err);
-    return {
-      userId: 0,
-      role: 0,
-    };
-  }
 }
 
 // checks if the current user is logged in
